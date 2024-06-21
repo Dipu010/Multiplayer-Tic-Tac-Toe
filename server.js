754b070fb9124fdd4b62eab47358ec36287a2c63
Ryzen_10 <115201346+Dipu010@users.noreply.github.com>
2024-06-21 12:14:11 +0530

    Domain changed

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import next from "next";
 import { Server } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
+const hostname = process.env.DOMAIN;
+const port = process.env.PORT
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
